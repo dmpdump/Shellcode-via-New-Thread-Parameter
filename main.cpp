ea3a3de97d99cec0ae39c4fe7755b4c6a7041be9
dmpdump <dmpdump@tuta.io>
2024-12-30 16:14:23 -0500

    Update main.cpp
    Added disclaimer

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Disclaimer:
+ * This code was created for educational purposes only.
+ * The author does not take responsibility for any misuse 
+ * or unintended consequences arising from its application.
+ * Users are encouraged to exercise caution and adhere to 
+ * all relevant laws and regulations when utilizing this code.
+ */
+
 #include <windows.h>
 #include <stdio.h>
 
